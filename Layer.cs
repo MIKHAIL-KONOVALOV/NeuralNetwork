19416fe016ecccfb40d62b4419d70e65ed6489b2
MIKHAIL-KONOVALOV <miklggwork@gmail.com>
2023-09-15 02:39:35 +0300

    Начало написания общего прогона данных в методе FeedForward

---
+++ Layer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    public class Layer
+    {
+        public List<Neuron> Neurons { get;}
+        public int Count => Neurons ?.Count ?? 0;
+
+        public Layer(List<Neuron> neurons, NeuronType type = NeuronType.Normal)
+        {
+            // TODO Проверить все входные нейроны на соответствие типу
+
+            Neurons = neurons;
+        }
+    }
+}
